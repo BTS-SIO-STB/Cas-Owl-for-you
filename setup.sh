43a58d91da1d23c671c16ccf9fe27df876b6a4d0
Mirage <clementbobin21@gmail.com>
2024-11-08 14:59:25 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ docker-compose ps
 
 # Wait for the SQL Server to fully start
 echo "Waiting for the SQL Server to initialize..."
-sleep 20  # Adjust this delay if SQL Server takes longer to start up
+sleep 200  # Adjust this delay if SQL Server takes longer to start up
 
 # Copy the setup script into the container
 echo "Copying the database setup script..."
@@ -18,8 +18,7 @@ docker cp ./OwlCreate.sql mssqlserver:/OwlCreate.sql
 
 # Install sqlcmd in the container and run the SQL script
 echo "Installing sqlcmd and running database setup..."
-docker exec mssqlserver /bin/bash -c "apt-get update && apt-get install -y mssql-tools unixodbc-dev"
-docker exec mssqlserver /bin/bash -c "/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P 'Password123456789' -d master -i /OwlCreate.sql -N -C"
+docker exec mssqlserver /bin/bash -c "/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P 'Password123456789' -d master -i /OwlCreate.sql -C"
 
 echo "Database setup complete."
 
